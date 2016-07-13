ff4607c89614815d51a55fd11f52911e0bdfd917
Axji <axel.zenklusen@gmail.com>
2016-07-13 08:14:07 +0200

    increment

--- Weather/GetWeatherData.py
+++ Weather/GetWeatherData.py
@@ -1,34 +1,44 @@
 from urllib import request
 import configparser
 from global_lib import *
+import re
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-opener = request.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
-wetterStationen = ['SIO', 'BER', 'BAS', 'CHM', 'CHD', 'GSB',
-                   'DAV', 'ENG', 'GVE', 'LUG', 'PAY', 'SIA',
-                   'SAE', 'SMA']
+def get_files_from_web():
+    opener = request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    wetter_stations = ['SIO', 'BER', 'BAS', 'CHM', 'CHD', 'GSB',
+                       'DAV', 'ENG', 'GVE', 'LUG', 'PAY', 'SIA',
+                       'SAE', 'SMA']
+    cfg_base_url = config['DEFAULT']['baseUrl']
+    cfg_string_to_replace_in_url = config['DEFAULT']['stringToReplaceInUrl']
+    cfg_sleep_time_between_files = int(config['DEFAULT']['sleepTimeBetweenFiles'])
 
-cfgBaseUrl = config['DEFAULT']['baseUrl']
-cfgstringToReplaceInUrl = config['DEFAULT']['stringToReplaceInUrl']
-cfgsleepTimeBetweenFiles = int(config['DEFAULT']['sleepTimeBetweenFiles'])
+    date_start = GlobalLib.date_iso()
+    # datetime.datetime.now().isoformat().replace('.', '_').replace(':', '')
+    for wetter_station in wetter_stations:
+        actual_url = cfg_base_url.replace(cfg_string_to_replace_in_url, wetter_station)
+        print("Actual File ="+ actual_url)
+        response = opener.open(actual_url)
+        # print(response.read())
+        f = open('daten\\' + date_start + '_' + wetter_station + '.txt', 'w')
+        response_str = response.read().decode("utf-8")
+        # Jegliche art von Leerzeilen entfernen (Anscheinend gibt es ein Problem mit \r\r\n daher zuerst Fehlerkorrektur
+        response_str = response_str.replace("\r\n", "\n")
+        response_str = response_str.replace("\n\n", "\n")
+        response_str = response_str.replace("\n\n", "\n")
+        #   filtered = os.linesep.join([s for s in responseStr.splitlines(True) if s.strip("\r\n")])
+        f.write(response_str)
+        time.sleep(cfg_sleep_time_between_files)
 
-print(cfgBaseUrl)
+def parse_files():
 
-DateStart = GlobalLib.date_iso()
-    #datetime.datetime.now().isoformat().replace('.', '_').replace(':', '')
 
-for wetterstation in wetterStationen:
-    #actualUrl =
-    response = opener.open(cfgBaseUrl.replace(cfgstringToReplaceInUrl, wetterstation))
-    # print(response.read())
-    f = open('daten\\'+DateStart+'_'+wetterstation+'.txt', 'w')
-    f.write(response.read().decode("utf-8"))
-    time.sleep(cfgsleepTimeBetweenFiles)
-
+get_files_from_web()
+parse_files()
 
 
 

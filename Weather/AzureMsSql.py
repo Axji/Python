215a517ef78b57d29ff81495735834f17c5d0ad6
Axji <axel.zenklusen@gmail.com>
2023-02-07 00:26:42 +0100

    Update AzureMsSql.py

--- Weather/AzureMsSql.py
+++ Weather/AzureMsSql.py
@@ -1,5 +1,5 @@
 import pymssql
 
 conn = pymssql.connect(server='ax1.database.windows.net', user='axji',
-                       password='1qayxsw2', database='AxjiDB1')
+                       password='XXX', database='AxjiDB1')
 
